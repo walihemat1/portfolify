760f6501e1b71433dd32abfe7cc4f18a3e4dd5d1
Ahmad Wali Hemat <ahmadwalihemat67@gmail.com>
2025-04-20 23:51:30 +0430

    add custom colors

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,5 +1,5 @@
 function App() {
-  return <div className="text-white bg-blue-300 text">App</div>;
+  return <div className="text-white bg-brand-accent text">App</div>;
 }
 
 export default App;
--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -1,8 +1,20 @@
 /** @type {import('tailwindcss').Config} */
-export default {
-  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+// tailwind.config.js
+module.exports = {
+  darkMode: "class", // Enables "dark" class switching
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        brand: {
+          primary: "#10B981", // Electric Green
+          background: "#0F172A", // Dark Gray / Navy
+          surface: "#1E293B", // Card/Modal Background
+          text: "#E2E8F0", // Soft White
+          accent: "#FACC15", // Yellow Accent
+        },
+      },
+    },
   },
   plugins: [],
 };
