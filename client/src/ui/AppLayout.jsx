04a021efe5d98b9eed9cf3ac2d57f41ea89f95e0
Ahmad Wali Hemat <ahmadwalihemat67@gmail.com>
2025-04-26 23:18:23 +0430

    clean AppLayout

--- client/src/ui/AppLayout.jsx
+++ client/src/ui/AppLayout.jsx
@@ -2,24 +2,6 @@ import Sidebar from "./Sidebar";
 import Topbar from "./Topbar";
 import BuilderCanvas from "./BuilderCanvas";
 
-// function AppLayout() {
-//   return (
-//     <div className="flex h-screen">
-//       <aside className="w-52">
-//         <Sidebar />
-//       </aside>
-//       <div className="flex flex-col flex-1">
-//         <div className="h-16">
-//           <Topbar />
-//         </div>
-//         <main className="flex-1 overflow-y-auto">
-//           <BuilderCanvas />
-//         </main>
-//       </div>
-//     </div>
-//   );
-// }
-
 function AppLayout() {
   return (
     <div className="grid grid-cols-[20rem_1fr] grid-rows-[auto_1fr] h-screen bg-background-light dark:bg-background-dark">
